826b85625f55e72b1c9f1c66443bb8312ca9f460
addycoderepo <adityagitcode@gmail.com>
2022-05-19 06:28:33 -0400

    html,css and js basics file are added

---
+++ basic.js
@@ -0,0 +1,10 @@
+// Variable Declaration with Var, hositing for var is global scope
+var name = "peter";
+console.log(name);
+
+//variable declaration with let and const, hoisting both let and const are block scope
+let place= "someWhere";
+console.log(place);
+
+const country ="someWhere";
+console.log(country);
